5bcf26ebc5dd2f1680d44df4c0257ceff9a22b4f
jude-LafiteIII <judewrs@gmail.com>
2018-09-03 12:41:09 +0100

    functionality

--- main.py
+++ main.py
@@ -1,15 +1,65 @@
-from models import session, Reminder
+from models import session, Reminder, Deletes
 import time
 import requests
+from datetime import datetime
+import configparser
 
+
+config = configparser.SafeConfigParser()
+config.read('config.ini')
 
 while True:
 
-    for reminder in session.query(Reminder).filter(Reminder.time <= time.time()):
+    rems = []
+    for reminder in session.query(Reminder).filter(Reminder.time <= time.time()).filter(Reminder.webhook != ''):
 
-        print('Reminder going')
+        url_w = reminder.webhook + '?wait=true'
+        url = reminder.webhook
 
-        requests.post(reminder.webhook, {'content': reminder.message, 'name': 'Reminders', 'avatar_url': 'https://raw.githubusercontent.com/reminder-bot/logos/master/Remind_Me_Bot_Logo_PPic.jpg'})
-        session.query(Reminder).filter(Reminder.id == reminder.id).delete(synchronize_session='fetch')
+        rems.append(reminder.id)
+        print('Looping for reminder {}'.format(reminder))
+
+        if reminder.interval is not None and reminder.interval < 8:
+            continue
+
+        try:
+            if reminder.interval is None:
+                requests.post(url, {'content': reminder.message, 'name': 'Reminders', 'avatar_url': 'https://raw.githubusercontent.com/reminder-bot/logos/master/Remind_Me_Bot_Logo_PPic.jpg'})
+
+                print('{}: Administered reminder to {}'.format(datetime.utcnow().strftime('%H:%M:%S'), reminder.webhook))
+
+            else:
+                rems.remove(reminder.id)
+
+                if reminder.message.startswith('-del_after_'):
+
+                    chars = ''
+
+                    for char in reminder.message[len('-del_after_'):]:
+                        if char in '0123456789':
+                            chars += char
+                        else:
+                            break
+
+                    wait_time = int(chars)
+
+                    t = requests.post(url_w, {'content': reminder.message, 'name': 'Reminders', 'avatar_url': 'https://raw.githubusercontent.com/reminder-bot/logos/master/Remind_Me_Bot_Logo_PPic.jpg'})
+
+                    d = Deletes(time=time.time() + wait_time, channel=message.channel.id, message=t['id'])
+
+                else:
+                    requests.post(url, {'content': reminder.message, 'name': 'Reminders', 'avatar_url': 'https://raw.githubusercontent.com/reminder-bot/logos/master/Remind_Me_Bot_Logo_PPic.jpg'})
+
+                print('{}: Administered interval to {} (Reset for {} seconds)'.format(datetime.utcnow().strftime('%H:%M:%S'), reminder.webhook, reminder.interval))
+
+                while reminder.time <= time.time():
+                    reminder.time += reminder.interval ## change the time for the next interval
+
+        except Exception as e:
+            print('Ln 1033: {}'.format(e))
+
+    if len(rems) > 0:
+        session.query(Reminder).filter(Reminder.id.in_(rems)).delete(synchronize_session='fetch')
 
     time.sleep(2.5)
+    session.commit()
--- models.py
+++ models.py
@@ -1,5 +1,5 @@
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer, BigInteger, Unicode, String
+from sqlalchemy import Column, Integer, BigInteger, Unicode, String, Text
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 import configparser
@@ -21,15 +21,29 @@ class Reminder(Base):
 
     id = Column(Integer, primary_key=True, unique=True)
     message = Column(Unicode(2000))
-    webhook = Column(String(200))
     channel = Column(BigInteger)
+    guild = Column(BigInteger)
     time = Column(BigInteger)
     interval = Column(Integer)
+
+    webhook = Column(String(200))
+    avatar = Column(Text)
 
     mysql_charset = 'utf8mb4'
 
     def __repr__(self):
         return '<Reminder "{}" <#{}> {}s>'.format(self.message, self.channel, self.time)
+
+
+
+class Deletes(Base):
+    __tablename__ = 'deletes'
+
+    map_id = Column(Integer, primary_key=True)
+    message = Column(BigInteger)
+    channel = Column(BigInteger)
+    time = Column(BigInteger)
+
 
 if passwd:
     engine = create_engine('mysql+pymysql://{user}:{passwd}@{host}/{db}?charset=utf8mb4'.format(user=user, passwd=passwd, host=host, db=database))
